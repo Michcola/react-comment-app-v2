dd13a6dc3ff18c1ee741c5e8e679a98b0ef2077c
mich <843298603@qq.com>
2019-05-11 12:52:19 +0800

    commentapp v2

---
+++ build/precache-manifest.6d2af69bd5e21330cd1674f10b20a291.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a33b5ca41098035ef21ac099bb706218",
+    "url": "/react-comment-app-v2/build/index.html"
+  },
+  {
+    "revision": "3e82ccfb0d53926203b0",
+    "url": "/react-comment-app-v2/build/static/css/main.50665814.chunk.css"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/react-comment-app-v2/build/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "3e82ccfb0d53926203b0",
+    "url": "/react-comment-app-v2/build/static/js/main.4f99ce97.chunk.js"
+  },
+  {
+    "revision": "7c3aac8112b1ca791ed9",
+    "url": "/react-comment-app-v2/build/static/js/runtime~main.8fa94155.js"
+  }
+]);
